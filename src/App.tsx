d5470aa71d3d97b475e3621920d1c00d840e4624
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-04 20:06:28 +0530

    Project title given 👀

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <h2 style={{ color: "orange" }}>Hotel App</h2>
     </div>
   );
 }
