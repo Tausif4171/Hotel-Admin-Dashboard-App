cb9df7dd618ce3cb8ccaa317661cad3a713d516f
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-08 23:51:38 +0530

    bug fixed🐛

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Navbar/>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
