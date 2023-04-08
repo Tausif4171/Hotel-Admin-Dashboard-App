bfa2c0659b4606f75ce6bf107c71bae5d8481a2e
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-08 23:21:57 +0530

    routes setup ✔

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Card from "./components/Card";
+import Create from "./components/Create";
+import Details from "./components/Details";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
       <h2 style={{ color: "orange" }}>Hotel App</h2>
+      <Routes>
+        <Route path='/' element={<Card />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/hotels/:id" element={<Details />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
