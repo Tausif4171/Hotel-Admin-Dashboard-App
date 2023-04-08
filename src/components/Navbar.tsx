a47a933b8c58be183170bcb3e4cc70b1a7daba59
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-09 00:29:22 +0530

    style given to navbar 🎨

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,31 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <>
-            <nav>
-                <h3>Dashboard</h3>
-                <div>
-                    <Link to='/'>
-                        <h4>All Hotels</h4>
+            <nav style={{ display: "flex", justifyContent: 'space-between', margin: "10px 60px 10px 60px" }}>
+                <div style={{ display: "flex" }}>
+                    <h2>Dashboard</h2>
+                </div>
+                <div style={{ display: 'flex', gap: "16px" }}>
+                    <Link to='/' style={{ textDecoration: 'none' }}>
+                        <h4 style={{
+                            textDecoration: 'none',
+                            color: 'grey'
+                        }}>All Hotels</h4>
                     </Link>
-                    <Link to='/create'>
-                        <h4>New Hotel</h4>
+                    <Link to='/create' style={{ textDecoration: 'none' }}>
+                        <h4 style={{
+                            color: 'grey',
+                            background: '#323130',
+                            borderRadius: '20px',
+                            padding: '0px 10px 0px 10px',
+                            height: '50px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            marginTop: '6px'
+                        }}>New Hotel</h4>
                     </Link>
                 </div>
-            </nav>
+            </nav >
         </>
     )
 }
