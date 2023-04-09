3a9ba1c8513473ec08c96f6f20c500a32d55a93c
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-09 23:54:08 +0530

    firebase sync with our app

---
+++ src/lib/Firebase.tsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCicJOM9M0ilvyGpXSlxqbP9Klbh_7z-gU",
+  authDomain: "hotel-dashboard-843e7.firebaseapp.com",
+  projectId: "hotel-dashboard-843e7",
+  storageBucket: "hotel-dashboard-843e7.appspot.com",
+  messagingSenderId: "540694919585",
+  appId: "1:540694919585:web:78a5aaa85600e3c129f3e0",
+  measurementId: "G-CXZJJ5GHF2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
