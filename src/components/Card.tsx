aec78ffaf385779c73734885c013c5f956b5a1d0
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-14 11:00:18 +0530

    hotels data types given ⌨

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { onSnapshot } from 'firebase/firestore'
+import { DocumentData, QuerySnapshot, onSnapshot } from 'firebase/firestore'
 import { hotelsCollection } from '../lib/Controller'
+import { NewHotelType } from '../types/hotels'
 
 export default function Card() {
-  const [hotels, setHotels] = useState([])
+  const [hotels, setHotels] = useState<NewHotelType[]>([])
   useEffect(() => {
-    onSnapshot(hotelsCollection, (snapshot) => {
+    onSnapshot(hotelsCollection, (snapshot: QuerySnapshot<DocumentData>) => {
       console.log('snapshot', snapshot.docs)
       setHotels(snapshot.docs.map((doc) => {
         console.log('doc', doc)
---
+++ src/types/hotels.ts
@@ -0,0 +1,13 @@
+export interface NewHotelType {
+    id?: string,
+    review?: string,
+    fearure?: string,
+    stars?: string,
+    country?: string,
+    region?: string,
+    title?: string,
+    location?: string,
+    totalPrice?: string,
+    perNight?: string,
+    description?: string
+}
