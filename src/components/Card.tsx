18172f35bfda48637a91cacaa31ea950e6a4f918
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-14 10:15:03 +0530

    snapshot method firebase 📷

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { onSnapshot } from 'firebase/firestore'
 
 export default function Card() {
+  useEffect(() => {
+    onSnapshot(hotelsCollection,()=>{
+      
+    })
+  })
   return (
     <div>
       <h3 style={{ color: "orange" }}>All Hotels</h3>
