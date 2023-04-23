6213f8351671111fc0759c002473d7e7fc3b541d
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-23 22:57:18 +0530

    main feature from database and button added for more info 🪶

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -17,6 +17,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
 
 interface IProps {
     hotel: NewHotelType
@@ -65,64 +66,37 @@ export default function Information({ hotel }: IProps) {
                         <span>Per Night</span>
                     </div>
                 }
-
                 title={hotel.title}
                 subheader={hotel.region}
             />
+
             <CardMedia
                 className={classes.media}
                 image="https://www.tourmyindia.com/blog//wp-content/uploads/2022/10/Best-Five-Star-Luxury-Hotels-in-Delhi.jpg"
                 title="five star hotel"
             />
+
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
                     {hotel.description}
                 </Typography>
             </CardContent>
+
             <CardActions disableSpacing>
-                <span style={{backgroundColor:'grey'}}>{hotel.stars} {" Stars "}</span>&nbsp;
+                <span style={{ backgroundColor: 'grey' }}>{hotel.stars} {" Stars "}</span>&nbsp;
                 <span>based on {hotel.review} reviews</span>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
-                <IconButton
-                    className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                    })}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </IconButton>
             </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                        minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                        again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don’t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
+
+            <CardActions disableSpacing>
+                <span>Main feature: {hotel.fearure}</span>
+            </CardActions>
+
+
+            <Button variant="contained" color="primary">
+                View more Information
+            </Button>
+
+
         </Card>
     );
 }
