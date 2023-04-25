c1dac254e1e3d1cc28d36605ba5b6825b63d30dd
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-25 21:24:43 +0530

    fetch a single document from firestore ❤️‍🔥

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { firestore } from '../lib/Controller'
 
 export default function Details() {
-  const {id} = useParams()
-  console.log({id})
+  const { id } = useParams()
+
+  // Fetch a single document
+  const getHotel = doc(firestore, `hotels/${id}`)
+  console.log({ id }, { getHotel })
+
+  const [hotel, setHotel] = useState({})
+  useEffect(() => {
+    const fetchHotelData = async () => {
+      const docSnap = await getDoc(getHotel)
+      if (docSnap.exists()) {
+        const newHotelObj = {
+          id: docSnap.id,
+          ...docSnap.data()
+        }
+        setHotel(newHotelObj)
+      }
+    }
+    fetchHotelData()
+  },[])
+  console.log({ id }, { getHotel },{hotel})
+  
   return (
     <div>Details</div>
   )
