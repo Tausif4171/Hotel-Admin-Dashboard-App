a854188213a6f76cfe6f9510fd6721100ef34eac
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-25 20:54:24 +0530

    useParams access id of that particular hotel 💡

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Details() {
+  const {id} = useParams()
+  console.log({id})
   return (
     <div>Details</div>
   )
