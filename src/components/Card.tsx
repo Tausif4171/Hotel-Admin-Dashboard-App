e576aea52bf6678cffbf461d07bb88f0331d3a21
Tausif4171 <tausifkhan4171@gmail.com>
2023-04-26 23:10:41 +0530

    style given to the card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,7 +20,7 @@ export default function Card() {
   }, [])
   // console.log('hotels', hotels)
   return (
-    <div>
+    <div style={{margin:'10px 60px 10px 60px'}}>
       <h3 style={{ color: "orange" }}>All Hotels</h3>
       {/* {
         hotels.map((item) => {
