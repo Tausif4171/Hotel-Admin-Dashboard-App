299c6ee161b55f3ef4bfad88d56d80ceb95bc620
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-01 20:49:00 +0530

    update edit component with joy UI library

--- src/components/Edit.tsx
+++ src/components/Edit.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Textarea from '@mui/joy/Textarea';
+
 
 export default function Edit() {
-  return (
-    <>
-    <div style={{border:'5px solid grey', display:'grid'}}>
-        <label>Please enter the new description below: </label>
-        <textarea />
-        <button>Update</button>
-    </div>
-    </>
-  )
+    const [italic, setItalic] = React.useState(false);
+    const [fontWeight, setFontWeight] = React.useState('normal');
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    return (
+        <FormControl>
+            <FormLabel>Please enter the new description below:</FormLabel>
+            <Textarea
+                placeholder="Type something here…"
+                minRows={2}
+                endDecorator={
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            gap: 'var(--Textarea-paddingBlock)',
+                            pt: 'var(--Textarea-paddingBlock)',
+                            borderTop: '1px solid',
+                            borderColor: 'divider',
+                            flex: 'auto',
+                        }}
+                    >
+
+
+                        <Button sx={{ ml: 'auto' }}>Update</Button>
+                    </Box>
+                }
+                sx={{
+                    minWidth: 300,
+                    fontWeight,
+                    fontStyle: italic ? 'italic' : 'initial',
+                }}
+            />
+        </FormControl>
+    );
 }
