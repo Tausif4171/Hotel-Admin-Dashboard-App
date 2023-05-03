5ac571425954fda5a0fb7e131c5ea49cff9989ab
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-03 23:20:27 +0530

    navigate used in create component 📛

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Create() {
   const [title, setTitle] = useState('')
   console.log(title)
-  function addHotel(e:React.FormEvent<HTMLFormElement>) {
+  const navigate = useNavigate()
+  function addHotel(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
+    navigate('/')
   }
   return (
     <div>
       <h3>Add a new Hotel</h3>
       <form onSubmit={(e) => addHotel(e)}>
         <label>Title</label>
-        <input type='text' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+        <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
         <button>Add Hotel</button>
       </form>
     </div>
