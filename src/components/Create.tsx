10dd23e24e425162da911c4e97ce12a88b1ca49c
Tausif4171 <tausifkhan4171@gmail.com>
2023-05-03 23:13:26 +0530

    form with title and handle loading after submitting 🎃

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Create() {
+  function addHotel(e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+  }
   return (
-    <div>Create</div>
+    <div>
+      <h3>Add a new Hotel</h3>
+      <form onSubmit={(e) => addHotel(e)}>
+        <label>Title</label>
+        <input type='text' />
+        <button>Add Hotel</button>
+      </form>
+    </div>
   )
 }
